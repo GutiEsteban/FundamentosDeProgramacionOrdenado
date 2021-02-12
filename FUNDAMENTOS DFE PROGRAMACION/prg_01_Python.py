da7b36fddecc951929cdd6aaa5603c6e8653d18a
GutiEsteban <78458343+GutiEsteban@users.noreply.github.com>
2021-02-12 10:56:59 -0500

    Add files via upload

---
+++ FUNDAMENTOS DFE PROGRAMACION/prg_01_Python.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file
+
+
+descripcion del ejercicio
+"""
+#linea de documentacion
+
+"""
+documentar varias lineas de codigo
+
+"""
+#codificacion
+
+
+# declaracion de variables
+
+
+#captura de datos
+
+
+#proceso que realizan los calculos
+
+
+
+#impresion de calculos
