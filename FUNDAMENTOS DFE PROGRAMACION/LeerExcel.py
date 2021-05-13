360042f42ba9cfacce54b3e8e462e1300136d63b
GutiEsteban <78458343+GutiEsteban@users.noreply.github.com>
2021-05-13 13:04:45 -0500

    Add files via upload

---
+++ FUNDAMENTOS DFE PROGRAMACION/LeerExcel.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 13 11:30:55 2021
+
+@author: gutie
+"""
+
+# Lectura de archivos tipo excel 
+# Importar librerías
+import pandas as pd 
+
+archivoExcel = pd.read_excel('Excel_1.xlsx')
+
+# Imprimir los datos
+print(archivoExcel)
